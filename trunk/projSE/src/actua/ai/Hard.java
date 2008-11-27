dc9ccddf5ced1687a6794028d80f40ba882e76c2
sibren.polders <sibren.polders@9a7a8d4a-9c33-11dd-ad9f-f77439c74b79>
2008-11-27 23:57:33 +0000

    JUnit tests Sibren

--- trunk/projSE/src/actua/ai/Hard.java
+++ trunk/projSE/src/actua/ai/Hard.java
@@ -1,4 +1,7 @@
 package actua.ai;
+import java.util.Vector;
+
+import actua.spelDelen.Tegel;
 import actua.types.Vector2D;
 
 public class Hard implements Strategy {
@@ -7,12 +10,13 @@ public class Hard implements Strategy {
 		
 	}
 
-	public Vector2D BerekenTegel () {
+	public Vector2D BerekenTegel (Vector<Vector<Tegel>> geplaatsteTegels, 
+									Vector<Tegel> stapel, Tegel tegel) {
 		return null;
 	}
 
-	@Override
-	public Vector2D BerekenPion() {
+	public Vector2D BerekenPion(Vector<Vector<Tegel>> geplaatsteTegels,
+			Vector2D laatstGeplaatst) {
 		// TODO Auto-generated method stub
 		return null;
 	}
--- trunk/projSE/src/actua/memento/StatusBijhouder.java
+++ trunk/projSE/src/actua/memento/StatusBijhouder.java
@@ -1,33 +1,61 @@
 package actua.memento;
 
 
-import java.util.ArrayList;
-
-
+import java.util.ArrayDeque;
 
 public class StatusBijhouder {
-	public ArrayList<Memento> redo;
-	public ArrayList<Memento> undo;
-
+	private static int MAX_SIZE = 100;
+	
+	private ArrayDeque<Memento> redo;
+	private ArrayDeque<Memento> undo;
+	
 	public StatusBijhouder() {
-		
+		undo = new ArrayDeque<Memento>();
+		redo = new ArrayDeque<Memento>();		
 	}
 
 	public void push_undo (Memento status) {
-		
-	}
-
-	public Memento pop_redo () {
-		return null;
+		push(undo, status);
 	}
 
 	public Memento pop_undo(){
-		return null;
+		return pop(undo);
 	}
-
+	
 	public void push_redo (Memento status) {
-		
+		push(redo, status);
+	}
+	
+	public Memento pop_redo () {
+		return pop(redo);
 	}
 
+	private Memento pop(ArrayDeque<Memento> stack) {
+		if (0 == stack.size()) {
+			return null;
+		}
+		
+		Memento ret = stack.getFirst();
+		stack.removeFirst(); // stack nabootsen, als er een Object wordt teruggeven is dit meteen ook verwijderd
+		return ret;
+	}
+	
+	private void push(ArrayDeque<Memento> stack, Memento status) {
+		if (MAX_SIZE == stack.size()) { // is de maximale stack grootte overschreden?
+			stack.removeLast();
+		}
+		
+		stack.addFirst(status);
+	}
+
+	// deze functies worden enkel in de unit tests gebruikt
+	public int getUndoSize() {
+		return undo.size();
+	}
+	
+	// deze functies worden enkel in de unit tests gebruikt
+	public int getRedoSize() {
+		return redo.size();		
+	}
 }
 
