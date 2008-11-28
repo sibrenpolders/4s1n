ddd9453f9ae1d3b6221f7639c7d78e02fb7085c9
staessenssibrand@hotmail.com <staessenssibrand@hotmail.com@9a7a8d4a-9c33-11dd-ad9f-f77439c74b79>
2008-11-28 10:36:27 +0000

    test SUITE

---
+++ projSE/src/testcases/AllTests.java
@@ -0,0 +1,23 @@
+package testcases;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite("Test for testcases");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(JunitCameraTest.class);
+		suite.addTestSuite(JunitTegelTest.class);
+		suite.addTestSuite(JunitTafelVerwerkerTest.class);
+		suite.addTestSuite(JunitTestSpel.class);
+		suite.addTestSuite(HardTest.class);
+		suite.addTestSuite(EasyTest.class);
+		suite.addTestSuite(JunitSpelerVerwerkerTest.class);
+		suite.addTestSuite(JunitTafelTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
