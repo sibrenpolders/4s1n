d91b65e64545016a6fd14acd01397049576c044a
n0123maker@gmail.com <n0123maker@gmail.com@9a7a8d4a-9c33-11dd-ad9f-f77439c74b79>
2008-11-28 01:12:03 +0000

    Junit Tafelverwerker

---
+++ trunk/projSE/src/actua/verwerkers/JunitTafelVerwerkerTest.java
@@ -0,0 +1,28 @@
+package actua.verwerkers;
+
+import actua.types.Vector3D;
+import junit.framework.TestCase;
+
+public class JunitTafelVerwerkerTest extends TestCase {
+	private TafelVerwerker tafelverwerker;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		tafelverwerker = new TafelVerwerker();
+	}
+	
+	public void herstelOverzicht() {
+		Vector3D herstel = new Vector3D(0,0,0);
+		
+		tafelverwerker.herstelOverzicht();
+		assertEquals("Overzicht hersteld", herstel, tafelverwerker.getOverzicht());
+	}
+	
+	public void testwijzigOverzicht() {
+		
+		Vector3D nieuwoverzicht = new Vector3D(5,6,7);
+		
+		tafelverwerker.wijzigOverzicht(nieuwoverzicht);
+		assertEquals("Overzicht gewijzigd", nieuwoverzicht, tafelverwerker.getOverzicht());
+	}
+}
