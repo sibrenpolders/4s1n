24a189c9ec92218926d9ffe224d42d34094ba89e
staessenssibrand@hotmail.com <staessenssibrand@hotmail.com@9a7a8d4a-9c33-11dd-ad9f-f77439c74b79>
2008-12-05 16:27:42 +0000

    consequent blijven

--- trunk/projSE/src/testcases/AllTests.java
+++ trunk/projSE/src/testcases/AllTests.java
@@ -9,16 +9,18 @@ public class AllTests {
 		TestSuite suite = new TestSuite("Test for testcases");
 		//$JUnit-BEGIN$
 		suite.addTestSuite(JunitAISpelerTest.class);
-		suite.addTestSuite(JunitCameraTest.class);
-		suite.addTestSuite(JunitTegelTest.class);
-		suite.addTestSuite(JunitTafelVerwerkerTest.class);
-		suite.addTestSuite(JunitTestSpel.class);
-		suite.addTestSuite(HardTest.class);
-		suite.addTestSuite(EasyTest.class);
-		suite.addTestSuite(JunitSpelerVerwerkerTest.class);
-		suite.addTestSuite(JunitTafelTest.class);
+		suite.addTestSuite(JunitAITest.class);
 		suite.addTestSuite(JunitBestandTest.class);
+		suite.addTestSuite(JunitCameraTest.class);
+		suite.addTestSuite(JunitEasyTest.class);
+		suite.addTestSuite(JunitHardTest.class);
 		suite.addTestSuite(JunitOptiesTest.class);
+		suite.addTestSuite(JunitTegelTest.class);
+		suite.addTestSuite(JunitSpelerVerwerkerTest.class);
+		//suite.addTestSuite(JunitStatusBijhouderTest.class);
+		suite.addTestSuite(JunitTafelTest.class);
+		suite.addTestSuite(JunitTafelVerwerkerTest.class);
+		suite.addTestSuite(JunitTegelTest.class);		
 		//$JUnit-END$
 		return suite;
 	}
