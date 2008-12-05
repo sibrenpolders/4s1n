b0bc022cf0a750e61ed123e6882acba75f00cb08
staessenssibrand@hotmail.com <staessenssibrand@hotmail.com@9a7a8d4a-9c33-11dd-ad9f-f77439c74b79>
2008-12-05 10:25:48 +0000

    Optie en OptieVerwerker 'zouden' moeten werken

--- trunk/projSE/src/actua/OptieVerwerker.java
+++ trunk/projSE/src/actua/OptieVerwerker.java
@@ -1,5 +1,11 @@
 package actua;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.security.InvalidParameterException;
 import java.util.Vector;
 
 public class OptieVerwerker {
@@ -8,16 +14,23 @@ public class OptieVerwerker {
 
 	public OptieVerwerker() {
 		opties = new Vector<Optie>();
+
+		try {
+			leesUitBestand(OPTIE_BESTAND);
+		} catch (IOException e) {
+			// <opties> blijft een lege vector
+		}
 	}
 
-	private void addOptie(Optie optie) {
-		opties.add(optie);
+	protected void finalize() throws Throwable {
+		super.finalize();
+		schrijfNaarBestand();
 	}
 
-	private void addOptie(String naam, String waarde) {
-		opties.add(new Optie(naam, waarde));
-	}
-
+	/**
+	 * @return null, als de optie met de gegeven naam niet bestaat; de waarde
+	 *         van die optie in het andere geval.
+	 */
 	public String getWaarde(String naam) {
 		for (int i = 0; i < opties.size(); ++i) {
 			Optie optie = opties.get(i);
@@ -26,6 +39,20 @@ public class OptieVerwerker {
 		}
 
 		return null;
+	}
+
+	public String getNaam(int i) {
+		if (i < 0 || i >= opties.size())
+			throw new IllegalArgumentException();
+
+		return opties.get(i).getNaam();
+	}
+
+	public String getWaarde(int i) {
+		if (i < 0 || i >= opties.size())
+			throw new IllegalArgumentException();
+
+		return opties.get(i).getWaarde();
 	}
 
 	private void setWaarde(String naam, String waarde) {
@@ -39,21 +66,26 @@ public class OptieVerwerker {
 		}
 
 		if (!veranderd)
-			addOptie(naam, waarde);
+			try {
+				addOptie(naam, waarde);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
 
-	public String getWaarde(int i) {
-		if (i < 0 || i >= opties.size())
-			throw new IllegalArgumentException();
+	private void addOptie(Optie optie) throws Exception {
+		if (getWaarde(optie.getNaam()) != null)
+			throw new Exception("Optie " + optie.getNaam()
+					+ " is reeds aanwezig.");
 
-		return opties.get(i).getWaarde();
+		opties.add(optie);
 	}
 
-	public String getNaam(int i) {
-		if (i < 0 || i >= opties.size())
-			throw new IllegalArgumentException();
+	private void addOptie(String naam, String waarde) throws Exception {
+		if (getWaarde(naam) != null)
+			throw new Exception("Optie " + naam + " is reeds aanwezig.");
 
-		return opties.get(i).getNaam();
+		opties.add(new Optie(naam, waarde));
 	}
 
 	public void veranderOptie(String[] nieuw) {
@@ -72,6 +104,69 @@ public class OptieVerwerker {
 		opties.clear();
 
 		for (int i = 0; i < nieuweOpties.length; ++i)
-			addOptie(nieuweOpties[i][0], nieuweOpties[i][1]);
+			veranderOptie(nieuweOpties[i]);
+	}
+
+	private class OptieParser {
+		public Optie zetOmInOptie(String regel)
+				throws InvalidParameterException {
+			String delims = ":";
+			String[] tokens = regel.split(delims);
+			if (!isGeldigeRegel(regel))
+				throw new InvalidParameterException();
+
+			return new Optie(tokens[0], tokens[2]);
+		}
+
+		public String zetOmInTekst(Optie o) {
+			return new String(o.getNaam() + "::" + o.getWaarde());
+		}
+
+		private boolean isGeldigeRegel(String regel) {
+			String delims = ":";
+			String[] tokens = regel.split(delims);
+			if (tokens.length < 3 || tokens[1].length() != 0)
+				return false;
+			else
+				return true;
+		}
+	}
+
+	private void leesUitBestand(String fileName) throws IOException {
+		if (opties == null || fileName == null)
+			throw new NullPointerException();
+
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+		String line = null;
+		OptieParser op = new OptieParser();
+		while ((line = reader.readLine()) != null) {
+			try {
+				Optie o = op.zetOmInOptie(line);
+				opties.add(o);
+			} catch (InvalidParameterException e) {
+				// Optie <o> wordt niet toegevoegd aan <opties>
+				;
+			}
+		}
+		reader.close();
+	}
+
+	public void schrijfNaarBestand() throws IOException {
+		schrijfNaarBestand(OPTIE_BESTAND);
+	}
+
+	public void schrijfNaarBestand(String fileName) throws IOException {
+		if (fileName == null)
+			throw new NullPointerException();
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+		OptieParser op = new OptieParser();
+
+		for (int i = 0; i < opties.size(); ++i) {
+			Optie o = opties.get(i);
+			String line = op.zetOmInTekst(o);
+			writer.write(line);
+		}
+		writer.close();
 	}
 }
