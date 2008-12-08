d771137586bd930d06ac6388172196022f3a1aaa
staessenssibrand@hotmail.com <staessenssibrand@hotmail.com@9a7a8d4a-9c33-11dd-ad9f-f77439c74b79>
2008-12-08 13:13:53 +0000

    factory was inderdaad veel te bloated

--- trunk/projSE/src/actua/Applicatie.java
+++ trunk/projSE/src/actua/Applicatie.java
@@ -1,28 +1,15 @@
 package actua;
 
 public class Applicatie {
-	// TODO
-	// Hebben deze globale variabelen ook maar enige nut??
-	private GFactory gFactory;
-	private Spel spel;
-	private HelpVerwerker help;
-	private OptieVerwerker opties;
-
 	public Applicatie(GFactory f) {
 		if (f == null) {
 			System.err.println("No graphical toolkit found.");
 			System.exit(1);
 		}
-		// TODO niet nodig??
-//		setGFactory(f);
-//		setHelp(new HelpVerwerker());
-//		setOpties(new OptieVerwerker());
-//		setSpel(new Spel());
 
-//		gFactory.showGWindow(getSpel(), getOpties(), getHelp());
-		
-		GWindow window = f.getWindow(new Spel(), new OptieVerwerker(), new HelpVerwerker());
-		window.show();		
+		GWindow window = f.getWindow(new Spel(), new OptieVerwerker(),
+				new HelpVerwerker());
+		window.show();
 	}
 
 	public static void main(String[] args) {
@@ -43,37 +30,5 @@ public class Applicatie {
 					+ " as a graphical toolkit is NOT supported.");
 			return null;
 		}
-	}
-
-	private final Spel getSpel() {
-		return spel;
-	}
-
-	private final void setSpel(Spel spel) {
-		this.spel = spel;
-	}
-
-	private final HelpVerwerker getHelp() {
-		return help;
-	}
-
-	private final void setHelp(HelpVerwerker help) {
-		this.help = help;
-	}
-
-	private final OptieVerwerker getOpties() {
-		return opties;
-	}
-
-	private final void setOpties(OptieVerwerker opties) {
-		this.opties = opties;
-	}
-
-	private final GFactory getGFactory() {
-		return gFactory;
-	}
-
-	private final void setGFactory(GFactory factory) {
-		gFactory = factory;
 	}
 }
