847ca4c12ec922adbcb1e1477f26809afc42de63
staessenssibrand@hotmail.com <staessenssibrand@hotmail.com@9a7a8d4a-9c33-11dd-ad9f-f77439c74b79>
2008-12-29 04:47:21 +0000

    bagger

--- trunk/projSE/src/actua/GTegel.java
+++ trunk/projSE/src/actua/GTegel.java
@@ -4,21 +4,26 @@ public abstract class GTegel {
 	protected String[] tegel;
 	int orientatie;
 	Spel spel;
-	
+
 	public GTegel() {
 		orientatie = 0;
 	}
-	
+
+	public GTegel(Spel spel) {
+		orientatie = 0;
+		this.spel = spel;
+	}
+
 	public GTegel(String[] tegel2, Spel spel) {
 		this.spel = spel;
-		this.tegel=tegel2;
+		this.tegel = tegel2;
 		if (tegel2.length == 3) {
 			orientatie = Integer.parseInt(tegel2[2]);
 		} else {
 			orientatie = 0;
 		}
 	}
-	
+
 	public abstract void roteer(boolean richting);
 
 	protected String[] getTegel() {
@@ -28,9 +33,8 @@ public abstract class GTegel {
 	protected void setTegel(String[] tegel) {
 		this.tegel = tegel;
 	}
-	
+
 	public int getOrientatie() {
 		return orientatie;
 	}
 }
-
